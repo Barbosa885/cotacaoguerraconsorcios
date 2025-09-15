abfa682f590b7c6d6c04a39c9905427ed6b5a067
Barbosa885 <gustavosports7@gmail.com>
2025-09-15 11:19:13 -0300

    feat: Shows the vehicle details after select brand, model and year

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { api } from "~/trpc/react";
 export default function Home() {
   const [selectedBrand, setSelectedBrand] = useState("");
   const [selectedModel, setSelectedModel] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
 
   // Hook do tRPC para chamar o procedimento getMarcas do "router" fipe
   const { data: brands, isLoading: isLoadingBrands, error } = api.fipe.getBrands.useQuery();
@@ -20,7 +21,10 @@ export default function Home() {
     { enabled: !!selectedBrand && !!selectedModel } // Só faz a requisição quando marca e modelo estiverem selecionadas
   );
 
-  if (isLoadingBrands) return <div>Carregando marcas...</div>;
+  const { data: vehicleDetails, isLoading: isLoadingVehicleDetails, error: vehicleDetailsError } = api.fipe.getPrice.useQuery(
+    { brandCode: selectedBrand, modelCode: selectedModel, yearCode: selectedYear },
+    { enabled: !!selectedBrand && !!selectedModel && !!years } // Só faz a requisição quando marca, modelo e ano estiverem selecionados
+  );
 
   if (error) return <div>Erro ao carregar marcas: {error.message}</div>;
 
@@ -49,7 +53,7 @@ export default function Home() {
       )}
 
       {selectedModel && (
-        <select>
+        <select onChange={(e) => setSelectedYear(e.target.value)}>
           <option>Selecione o Ano</option>
           {isLoadingYears && <option>Carregando...</option>}
           {years?.map((year) => (
@@ -59,6 +63,28 @@ export default function Home() {
           ))}
         </select>
       )}
+
+      {selectedYear && (
+        <div>
+          {isLoadingVehicleDetails && <p>Carregando detalhes do veículo...</p>}
+          {vehicleDetailsError && <p>Erro ao carregar detalhes do veículo: {vehicleDetailsError.message}</p>}
+          {vehicleDetails && (
+            <div>
+              <h2>Detalhes do Veículo</h2>
+              <p>Marca: {vehicleDetails.Marca}</p>
+              <p>Modelo: {vehicleDetails.Modelo}</p>
+              <p>Ano: {vehicleDetails.AnoModelo}</p>
+              <p>Combustível: {vehicleDetails.Combustivel}</p>
+              <p>Código Fipe: {vehicleDetails.CodigoFipe}</p>
+              <p>Preço: {vehicleDetails.Valor}</p>
+              <p>Mês de Referência: {vehicleDetails.MesReferencia}</p>
+              <p>Tipo de Combustível: {vehicleDetails.TipoVeiculo === 1 ? 'Carro' : vehicleDetails.TipoVeiculo === 2 ? 'Moto' : 'Caminhão'}</p>
+              <p>Símbolo de Combustível: {vehicleDetails.SiglaCombustivel}</p>
+            </div>
+          )}
+        </div>
+      )}
+
     </div>
   );
 }
